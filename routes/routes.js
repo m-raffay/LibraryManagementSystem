b662673acfd00863350527d5aeda9a59cdaf900b
Raffay <f200152@cfd.nu.edu.pk>
2023-03-27 22:51:15 +0500

    Merge branch 'main' of https://github.com/m-raffay/Online_Book_Store

--- routes/routes.js
+++ routes/routes.js
@@ -12,12 +12,55 @@ const authenticate = require("../controller/authenticate");
 // const addBook = require("../controller/user" );
 // router.route("/").get(addBook);
 
-router.get("/user", user.func);
-router.get("/books", books.func);
-router.get("/buy", buy.func);
-router.get("/admin", admin.func);
-router.get("/addbooksadmin", addbooksadmin.func);
-router.get("/showbookusr", showbooksusr.func);
-router.get("/deletebooks", deletebooks.func);
-router.get("/authenticate", authenticate.func);
+router.get("/bookbyid/:id", books.getBookById);
+router.delete("/deletebookbyid/:id", books.deleteBookById);
+router.get("/allbooks", books.getAllBooks);
+router.post("/createbook", books.createBook);
+router.put("/updatebookbyid/:id", books.updateBookById);
+
+router.get("/customerbyid/:id", customer.getCustomerById);
+router.delete("/deletecustomerbyid/:id", customer.deleteCustomerbyid);
+router.get("/allcustomers", customer.getAllCustomers);
+router.post("/createcustomer", customer.createCustomer);
+router.put("/updatecustomerbyid/:id", customer.updateCustomerbyid);
+
+router.get("/reviewbyid/:id", review.getReviewById);
+router.delete("/deletereviewbyid/:id", review.deleteReviewById);
+router.get("/allreview", review.getAllReviews);
+router.post("/createreview", review.createReview);
+router.put("/updatereviewbyid/:id", review.updateReviewById);
+
+
+router.get("/adminbyid/:id",admin.getAdminById);
+router.delete("/deletereadminbyid/:id",admin.deleteAdminById);
+router.get("/allreview", admin.getAllAdmins);
+router.post("/createadmin", admin.createAdmin);
+router.put("/updateadminbyid/:id", admin.updateAdminById);
+
+router.get("/orderbyid/:id",order.getOrderById);
+router.delete("/deletereorderbyid/:id",order.deleteOrderById);
+router.get("/allorder", order.getAllOrders);
+router.post("/createorder", order.createOrder);
+router.put("/updateorderbyid/:id", order.updateOrderById);
+
+router.get("/orderitemsbyid/:id",orderitem.getOrderItemById);
+router.delete("/deletereorderitemsbyid/:id",orderitem.deleteOrderItemById);
+router.post("/createorderitems", orderitem.createOrderItem);
+router.put("/updateorderitemsbyid/:id", orderitem.updateOrderItemById);
+
+
+router.post('/register', auth.register);
+router.post('/login', auth.login);
+router.get('/profile', authenticateToken, (req, res) => {
+  res.json(req.user);
+});
+
+
+// router.get("/user", user.func);
+// router.get("/buy", buy.func);
+// router.get("/admin", admin.func);
+// router.get("/addbooksadmin", addbooksadmin.func);
+// router.get("/showbookusr", showbooksusr.func);
+// router.get("/deletebooks", deletebooks.func);
+// router.get("/authenticate", authenticate.func);
 module.exports = router;
--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const app = express();
 const port = process.env.port || 3000;
 const userRoute = require("./routes/routes");
 
-app.use("/", userRoute);
-app.set("view engine", "hbs")
-app.use(express.static('public'))
-app.get("", function(req, res) {
-  res.render("index");
-});
+const route = require('./routes/routes');
+app.use('/', route);
+// app.use('/', (req,res) => {
+//   res.json ({
+//     message : "Sample"
+//   })
+// });
+
+// app.use("/", userRoute);
+// app.set("view engine", "hbs")
+// app.use(express.static('public'))
+// app.get("", function(req, res) {
+//   res.render("index");
+// });
 
 app.listen(port, function() {
   console.log("Server listening on port " + port);
